68b12bb02f8a1157c265ca09288288983f7b5535
ygyc2020-ops <ygyc2020@gmail.com>
2025-10-01 09:17:06 +0000

    나의 첫 번째 앱

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,3 @@ st.title("🎈정유찬의 첫 번째 앱")
 st.success("햄버거를 좋아해요")
 st.info("햄버거를 좋아해요")
 st.image("https://lh4.googleusercontent.com/proxy/xTq5-7nJUscVXvk8X8Qi3TqTzTNanVTt8f8rQWK9sQ0tuQmfcrfN4FTmq4OcACxNzo-hzCOCfaIzJVZirIClHOYpS2RBFL4bY2up6QfI2hS34ZhEhw")
-   
