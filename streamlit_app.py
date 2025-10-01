6aa8f4c41d09facd9a3e63a4ff06679de8d87e12
ygyc2020-ops <ygyc2020@gmail.com>
2025-10-01 09:14:13 +0000

    나의 첫 번째 앱

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈정유찬의 첫 번째 앱")
+
+st.success("햄버거를 좋아해요")
+st.info("햄버거를 좋아해요")
+st.image("https://lh4.googleusercontent.com/proxy/xTq5-7nJUscVXvk8X8Qi3TqTzTNanVTt8f8rQWK9sQ0tuQmfcrfN4FTmq4OcACxNzo-hzCOCfaIzJVZirIClHOYpS2RBFL4bY2up6QfI2hS34ZhEhw")
+   
